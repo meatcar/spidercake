6d544e15705e5d0e99aeab813bd8272291d034b6
Denys Pavlov <denys.pavlov@gmail.com>
2012-04-19 03:36:05 -0400

    made links display based on data given, added search box

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,24 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+      title: 'Spidercake',
+      links: [ 
+        { name: 'Gmail',
+          url: 'http://gmail.com/',
+          icon: 'icon-envelope' },
+        { name: 'Calendar',
+          url: 'http://calendar.google.com/', 
+          icon: 'icon-calendar' },
+        { name: 'Docs',
+          url: 'http://docs.google.com/', 
+          icon: 'icon-file' },
+        { name: 'Reddit',
+          url: 'http://reddit.com/', 
+          icon: 'icon-heart' },
+        { name: 'Netflix',
+          url: 'http://netflix.com/', 
+          icon: 'icon-film' }
+      ]
+  });
 };
